57f432d0f69cce390231fe775ec9a9e529a07cc5
Saurav-sanu <142467513+Saurav-sanu@users.noreply.github.com>
2025-07-05 21:30:04 +0530

    Update CopyEmailButton component

--- src/components/CopyEmailButton.jsx
+++ src/components/CopyEmailButton.jsx
@@ -5,7 +5,7 @@ import { AnimatePresence, motion } from 'motion/react';
 
 const CopyEmailButton = () => {
     const [copied, setCopied] = useState(false);
-    const email="Your Email Address";
+    const email="sauravsanu100@gmail.com";
 
     const copyToClipboard = () => {
         navigator.clipboard.writeText(email);
